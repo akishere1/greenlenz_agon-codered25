ffd90b7333cba0f3989def5967873e74730c5edf
arfath <arfathkhan063@gmail.com>
2025-01-10 04:54:15 +0530

    Revert " running server"
    This reverts commit b924bb37e192f3e95e750402d72c8e6ba52f7229.

--- models/user.js
+++ models/user.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
 // MongoDB Atlas connection string
-const connectionString = process.env.DB_CONNECTION_STRING || 'mongodb+srv://tryhariomsk:5zmmGkYobOq1RxBS@cluster0.jas61.mongodb.net/greenlens?retryWrites=true&w=majority';
+// const connectionString = process.env.DB_CONNECTION_STRING || 'mongodb+srv://tryhariomsk:5zmmGkYobOq1RxBS@cluster0.jas61.mongodb.net/?retryWrites=true&w=majority';
+const connectionString = process.env.DB_CONNECTION_STRING || 'mongodb+srv://tryhariomsk:5zmmGkYobOq1RxBS@cluster0.jas61.mongodb.net/?retryWrites=true&w=majority&tls=true';
 
 mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   dbName: 'greenlens', // Replace with your database name
-  tls: true, // Enable TLS (SSL) for secure connection
-  tlsAllowInvalidCertificates: false, // Do not disable certificate validation
+  tlsAllowInvalidCertificates: true, // Disable certificate validation temporarily
 })
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
-
+  
 // Define a sample schema and model
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -21,7 +23,6 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
 
 // // Connect to MongoDB Atlas
 // mongoose.connect(connectionString, {
